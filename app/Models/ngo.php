825d85ed0ca236d4189e210142a84b3e259cde88
ankitrathore31 <ankitrathore3110@gmail.com>
2025-05-10 17:42:59 +0530

    conflicts

--- app/Models/ngo.php
+++ app/Models/ngo.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ngo extends Model
+class Ngo extends Model
 {
     use HasFactory;
 }
