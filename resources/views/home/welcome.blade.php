9c5fe636dc6f9f5798747db706dd5af41014c92a
Ankit Rathore <ankitrathore3110@gmail.com>
2025-06-05 18:43:18 +0530

    Update welcome.blade.php

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -169,7 +169,7 @@
     <div class="owl-carousel owl-theme slider">
         <div class="item ">
             <div class="background-image">
-                <img src="{{ asset('images/sewing.jpeg') }}" alt="slider image">
+                <img src="https://srv1276-files.hstgr.io/e4aef852a4f6e947/files/public_html/public/images/ach1.jpeg" alt="slider image">
                 <img src="{{ asset('images/peace-talk.jpeg') }}" alt="slider image">
                 <div class="carousel-caption d-none d-md-block">
                     <h1 class="text-white"></h1>
