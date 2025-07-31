951dd4817aaa702f7472e7c990318db019090809
ankitrathore31 <ankitrathore3110@gmail.com>
2025-07-31 21:42:43 +0530

    Conflicts

---
+++ resources/views/home/pages/demand.blade.php
@@ -0,0 +1,18 @@
+@extends('home.layout.MasterLayout')
+@Section('content')
+<!-- Demand Section Start -->
+<section class="py-5">
+    <div class="container">
+        <div class="row g-4">
+            <!-- Demand Details -->
+            <div class="col-12 col-xl-6">
+                <div class="mb-4">
+                    <h2 class="fw-bold">Demand</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Demand Section End -->
+
+@endsection
--- resources/views/home/sidebar/sidebar.blade.php
+++ resources/views/home/sidebar/sidebar.blade.php
@@ -24,7 +24,7 @@
         <!-- Navbar Toggler for Mobile -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon" style="background-color: black;"></span>
         </button>
 
         <!-- Navbar Links -->
